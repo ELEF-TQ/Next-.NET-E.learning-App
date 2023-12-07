6174aacfc54c32ea9f279d998809233567bcd756
Abdessamad <tanafaat.wac.49@gmail.com>
2023-12-07 02:30:34 +0100

    organization:Auth chapters(reed lbal lendspoints f front)

--- Server/E_learningAppBack/Controllers/Chapters.cs
+++ Server/E_learningAppBack/Controllers/Chapters.cs
@@ -4,91 +4,16 @@ using Microsoft.EntityFrameworkCore;
 
 [Route("api/chapters")]
 [ApiController]
-public class MainController : ControllerBase
+public class Chapters : ControllerBase
 {
     private readonly e_learningContext _context;
 
-    public MainController(e_learningContext context)
+    public Chapters(e_learningContext context)
     {
         _context = context;
     }
 
-    [HttpPost("register")]
-    public async Task<ActionResult<User>> Register(UserRegistrationModel model)
-    {
-        try
-        {
-            if (await _context.Users.AnyAsync(u => u.Username == model.Username))
-            {
-                return BadRequest("Username is already taken");
-            }
-
-            var newUser = new User
-            {
-                Username = model.Username,
-                Password = model.Password,
-                Email = model.Email,
-            };
-
-            _context.Users.Add(newUser);
-            await _context.SaveChangesAsync();
-
-            return Ok(newUser);
-        }
-        catch (Exception ex)
-        {
-            return StatusCode(500, "An error occurred while processing your request.");
-        }
-    }
-
-
-    [HttpPost("login")]
-    public async Task<ActionResult<string>> Login(UserCredentials userCredentials)
-    {
-        try
-        {
-            var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == userCredentials.Email && u.Password == userCredentials.Password);
-
-            if (user == null)
-            {
-                return BadRequest("Invalid username or password");
-            }
-
-            var token = GenerateRandomToken();
-
-            return Ok(new
-            {
-                Token = token,
-                User = new
-                {
-                    Id = user.UserId,
-                    Username = user.Username,
-                    Email = user.Email,
-                    TotalScore = user.TotalScore
-                }
-            });
-        }
-        catch (Exception ex)
-        {
-            return StatusCode(500, $"An error occurred while processing the request : {ex.Message}");
-        }
-    }
-
-
-    private string GenerateRandomToken()
-    {
-        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        var random = new Random();
-        var token = new string(Enumerable.Repeat(chars, 32)
-          .Select(s => s[random.Next(s.Length)]).ToArray());
-
-        return token;
-    }
-
-
-
-
-    [HttpGet("chapterdata/{chapterId}")]
+    [HttpGet("{chapterId}")]
     public IActionResult GetQuizDataByChapter(int chapterId)
     {
         try
